34f35859801dce1354469b53a5481a2a8011c330
jreleaserbot <jreleaser@kordamp.org>
2021-05-04 13:11:38 -0700

    riot-gen v2.11.2

--- Formula/riot-gen.rb
+++ Formula/riot-gen.rb
@@ -1,9 +1,9 @@
 class RiotGen < Formula
   desc "Redis Input/Output Tools"
   homepage "https://developer.redislabs.com/riot"
-  version "2.11.1"
-  url "https://github.com/redis-developer/riot/releases/download/v2.11.1/riot-gen-2.11.1.zip"
-  sha256 "7e0584d78eca355756a8b64978664f9b9fce9633aa5977cbe00915778d470703"
+  version "2.11.2"
+  url "https://github.com/redis-developer/riot/releases/download/v2.11.2/riot-gen-2.11.2.zip"
+  sha256 "a7cf7e03b96c17fdf0e417eda628e14657aa8bbea0e1ad9509c88204b345c4d3"
   license "Apache-2.0"
 
   bottle :unneeded
@@ -17,6 +17,6 @@ class RiotGen < Formula
 
   test do
     output = shell_output("#{bin}/riot-gen --version")
-    assert_match "2.11.1", output
+    assert_match "2.11.2", output
   end
 end

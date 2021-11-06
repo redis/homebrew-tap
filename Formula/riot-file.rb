c137c8986161964b5315ed1c875154d4a6f32414
jreleaserbot <jreleaser@kordamp.org>
2021-11-06 22:25:07 +0000

    riot-file v2.14.0

--- Formula/riot-file.rb
+++ Formula/riot-file.rb
@@ -1,12 +1,10 @@
 class RiotFile < Formula
   desc "Redis Input/Output Tools"
   homepage "https://developer.redis.com/riot"
-  version "2.13.0"
-  url "https://github.com/redis-developer/riot/releases/download/v2.13.0/riot-file-2.13.0.zip"
-  sha256 "ec6b4af109ca4a344809e9844d05812dd44f8aeff12a232ee0ba13e77261d8ac"
+  version "2.14.0"
+  url "https://github.com/redis-developer/riot/releases/download/v2.14.0/riot-file-2.14.0.zip"
+  sha256 "d8a79350bd66bc11907cc078451d253d48845b8ca4764831b393742d7215febf"
   license "Apache-2.0"
-
-  bottle :unneeded
 
   depends_on "openjdk@8"
 
@@ -17,6 +15,6 @@ class RiotFile < Formula
 
   test do
     output = shell_output("#{bin}/riot-file --version")
-    assert_match "2.13.0", output
+    assert_match "2.14.0", output
   end
 end

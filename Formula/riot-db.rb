3181e10782bbb5e0cb25ff86b3bbdd674212daff
jreleaserbot <jreleaser@kordamp.org>
2021-12-06 22:54:40 +0000

    riot-db v2.14.3

--- Formula/riot-db.rb
+++ Formula/riot-db.rb
@@ -1,9 +1,9 @@
 class RiotDb < Formula
   desc "Redis Input/Output Tools"
   homepage "https://developer.redis.com/riot"
-  url "https://github.com/redis-developer/riot/releases/download/v2.14.2/riot-db-2.14.2.zip"
-  version "2.14.2"
-  sha256 "cde1f15ef57c0dd197e8c04186deb435261766ffb499125be945ba12ab97a10a"
+  url "https://github.com/redis-developer/riot/releases/download/v2.14.3/riot-db-2.14.3.zip"
+  version "2.14.3"
+  sha256 "3cba55923285a5a8f30b52619ba46c75e539c23161dca62d280ac75b7f5311b1"
   license "Apache-2.0"
 
   depends_on "openjdk@8"
@@ -15,6 +15,6 @@ class RiotDb < Formula
 
   test do
     output = shell_output("#{bin}/riot-db --version")
-    assert_match "2.14.2", output
+    assert_match "2.14.3", output
   end
 end

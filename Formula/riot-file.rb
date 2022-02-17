fe847d886c5c04ac1eebae28d322a4be01dbcbde
jreleaserbot <jreleaser@kordamp.org>
2022-02-17 00:42:07 +0000

    riot-file v2.15.4

--- Formula/riot-file.rb
+++ Formula/riot-file.rb
@@ -1,21 +1,21 @@
-# Generated with JReleaser 1.0.0-M1 at 2022-02-08T07:12:51.8421Z
+# Generated with JReleaser 1.0.0-M2 at 2022-02-17T00:40:24.343662Z
 class RiotFile < Formula
   desc "Redis Input/Output Tools"
   homepage "https://developer.redis.com/riot"
-  url "https://github.com/redis-developer/riot/releases/download/v2.15.2/riot-file-2.15.2.zip"
-  version "2.15.2"
-  sha256 "a2578f4b812586997a9f3614d0d24d280d5d055d6767306f025245a0f40e6039"
+  url "https://github.com/redis-developer/riot/releases/download/v2.15.4/riot-file-2.15.4.zip"
+  version "2.15.4"
+  sha256 "569cf71ad468759db5eadf77afee9c20b7d00d0b28f94a2b495990034ece3efe"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/riot-file"
+    bin.install_symlink "#{libexec}/bin/riot-file" => "riot-file"
   end
 
   test do
     output = shell_output("#{bin}/riot-file --version")
-    assert_match "2.15.2", output
+    assert_match "2.15.4", output
   end
 end

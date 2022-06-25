20dbee24cab4c69a785eec720db148a1f1e88e73
jreleaserbot <jreleaser@kordamp.org>
2022-06-25 18:44:39 +0000

    riot-stream v2.16.0

--- Formula/riot-stream.rb
+++ Formula/riot-stream.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.0.0 at 2022-05-26T05:20:05.406894Z
+# Generated with JReleaser 1.0.0 at 2022-06-25T18:40:04.260101Z
 class RiotStream < Formula
   desc "Redis Input/Output Tools"
   homepage "https://developer.redis.com/riot"
-  url "https://github.com/redis-developer/riot/releases/download/v2.15.5/riot-stream-2.15.5.zip"
-  version "2.15.5"
-  sha256 "36c5493dda36126f071d9c26e5f1a4ea057186fac9f09dc342e5da087e8c7781"
+  url "https://github.com/redis-developer/riot/releases/download/v2.16.0/riot-stream-2.16.0.zip"
+  version "2.16.0"
+  sha256 "dff33b998b006e80d0267cb9c5f0cf3c03100a3aeb5719712bcd4783713bee8e"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
@@ -16,6 +16,6 @@ class RiotStream < Formula
 
   test do
     output = shell_output("#{bin}/riot-stream --version")
-    assert_match "2.15.5", output
+    assert_match "2.16.0", output
   end
 end

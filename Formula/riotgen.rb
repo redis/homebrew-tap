080cdb7829512acf60bcb36fb89eaa1d4e5c176f
jreleaserbot <jreleaser@kordamp.org>
2023-01-21 07:17:09 +0000

    riot-gen v2.19.0

--- Formula/riotgen.rb
+++ Formula/riotgen.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.5.0-SNAPSHOT at 2023-01-21T07:01:33.780819408Z
+# Generated with JReleaser 1.5.0-SNAPSHOT at 2023-01-21T07:14:52.664595681Z
 class RiotGen < Formula
   desc "Redis Input/Output Tools"
   homepage "https://developer.redis.com/riot"
-  url "https://github.com/redis-developer/riot/releases/download/v2.9.0/riot-gen-2.9.0.zip"
-  version "2.9.0"
-  sha256 "82b4e1c621a61039a9110f7a262ed67a96c15f98471c2cc8b746f5817a9f1881"
+  url "https://github.com/redis-developer/riot/releases/download/v2.19.0/riot-gen-2.19.0.zip"
+  version "2.19.0"
+  sha256 "27eca5256858372f15eaa9d548ed4b31dfa47b5f9606eb590454b178f7cdd112"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
@@ -16,6 +16,6 @@ class RiotGen < Formula
 
   test do
     output = shell_output("#{bin}/riot-gen --version")
-    assert_match "2.9.0", output
+    assert_match "2.19.0", output
   end
 end

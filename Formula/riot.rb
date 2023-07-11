233ad1e5034f076131c71a968c957c888510c158
jreleaserbot <jreleaser@kordamp.org>
2023-07-11 08:26:01 +0000

    riot v3.1.3

--- Formula/riot.rb
+++ Formula/riot.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-05T23:49:08.84095428Z
+# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-11T08:23:56.551766451Z
 class Riot < Formula
   desc "Get data in and out of Redis with RIOT"
   homepage "https://github.com/redis-developer/riot"
-  url "https://github.com/redis-developer/riot/releases/download/v3.1.2/riot-3.1.2.zip"
-  version "3.1.2"
-  sha256 "2557e6d16662d626a133c887b5bc8e9560ad9dac9b969df13fca982dd5553ee5"
+  url "https://github.com/redis-developer/riot/releases/download/v3.1.3/riot-3.1.3.zip"
+  version "3.1.3"
+  sha256 "ea140b8211620513e9932d96a6f6b1a6c67ca502bd81d7210d0632b70cde7c67"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
@@ -16,6 +16,6 @@ class Riot < Formula
 
   test do
     output = shell_output("#{bin}/riot --version")
-    assert_match "3.1.2", output
+    assert_match "3.1.3", output
   end
 end

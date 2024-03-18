43a2f83ee562d5339036293f608600f85d3d9759
jreleaserbot <jreleaser@kordamp.org>
2024-03-18 22:41:44 +0000

    riot v3.2.0

--- Formula/riot.rb
+++ Formula/riot.rb
@@ -1,13 +1,14 @@
-# Generated with JReleaser 1.8.0-SNAPSHOT at 2023-08-16T17:07:46.198064826Z
+# Generated with JReleaser 1.11.0 at 2024-03-18T22:40:17.78948266Z
+
 class Riot < Formula
   desc "Get data in and out of Redis with RIOT"
   homepage "https://github.com/redis-developer/riot"
-  url "https://github.com/redis-developer/riot/releases/download/v3.1.5/riot-3.1.5.zip"
-  version "3.1.5"
-  sha256 "aabbc86ec33c4dd64ab3f73ea9166c51d29dcce3ad168a6a59ceba498e444428"
+  url "https://github.com/redis-developer/riot/releases/download/v3.2.0/riot-3.2.0.zip"
+  version "3.2.0"
+  sha256 "fb39ce2c19e31ea6f9d6d3dede35d0da6408a41301e4783115d21f90e5ff7b1b"
   license "Apache-2.0"
 
-  depends_on "openjdk@17"
+  depends_on "openjdk@21"
 
   def install
     libexec.install Dir["*"]
@@ -16,6 +17,6 @@ class Riot < Formula
 
   test do
     output = shell_output("#{bin}/riot --version")
-    assert_match "3.1.5", output
+    assert_match "3.2.0", output
   end
 end

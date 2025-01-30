fcf457f0df2e85286b89709a45be5125881e6838
jreleaserbot <jreleaser@kordamp.org>
2025-01-30 02:41:14 +0000

    riot v4.2.2

--- Formula/riot.rb
+++ Formula/riot.rb
@@ -1,26 +1,26 @@
-# Generated with JReleaser 1.16.0 at 2025-01-29T02:01:24.252931461Z
+# Generated with JReleaser 1.16.0 at 2025-01-30T02:36:28.945873482Z
 
 class Riot < Formula
   desc "Get data in and out of Redis with RIOT"
   homepage "https://github.com/redis/riot"
-  version "4.2.1"
+  version "4.2.2"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/redis/riot/releases/download/v4.2.1/riot-standalone-4.2.1-linux-aarch64.zip"
-    sha256 "b2e36b7ea870b3f8edf26835bd6384df608c113502b3dc851e2dd8f5a1c72be7"
+    url "https://github.com/redis/riot/releases/download/v4.2.2/riot-standalone-4.2.2-linux-aarch64.zip"
+    sha256 "995ffadf80b68cd6fcef1cdfb487e0ba9ec816daba76589cf21e14d066c12185"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/redis/riot/releases/download/v4.2.1/riot-standalone-4.2.1-linux-x86_64.zip"
-    sha256 "f6a8fea0cb7a0136b7f85fe57df6a00f0c4be433bafe76456eb31b9134ebd880"
+    url "https://github.com/redis/riot/releases/download/v4.2.2/riot-standalone-4.2.2-linux-x86_64.zip"
+    sha256 "3a749ade742ed33dd536e70bdcd4d5e2d2bae30553a7904961b4c51834797f8d"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/redis/riot/releases/download/v4.2.1/riot-standalone-4.2.1-osx-aarch64.zip"
-    sha256 "d2015c97d5f375784b5a242ec154b88649a4df9b2122711986a54197951ff83f"
+    url "https://github.com/redis/riot/releases/download/v4.2.2/riot-standalone-4.2.2-osx-aarch64.zip"
+    sha256 "bb1c65546d7eb79a5968123edf24fefd51bed4d2a693ed7279425536c9c4e59c"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/redis/riot/releases/download/v4.2.1/riot-standalone-4.2.1-osx-x86_64.zip"
-    sha256 "464f6b63adb911fa55839fbce25d6bad99cc1e9923fca7acdc9489fac86597bf"
+    url "https://github.com/redis/riot/releases/download/v4.2.2/riot-standalone-4.2.2-osx-x86_64.zip"
+    sha256 "193816bdd31bc3e10d022db2ba8eb2a3f7fb951c728a562fa51a5388f0d237ee"
   end
 
 
@@ -42,6 +42,6 @@ class Riot < Formula
 
   test do
     output = shell_output("#{bin}/riot --version")
-    assert_match "4.2.1", output
+    assert_match "4.2.2", output
   end
 end

61bda89cda81137164b3bd0d6bf1a2dab1995821
Julien Ruaux <jruaux@users.noreply.github.com>
2025-02-20 13:47:29 -0800

    Fixed download links

--- Formula/riotx.rb
+++ Formula/riotx.rb
@@ -7,19 +7,19 @@ class Riotx < Formula
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/redis-field-engineering/riotx/releases/download/v0.4.2/riotx-standalone-0.4.2-linux-aarch64.zip"
+    url "https://github.com/redis-field-engineering/riotx-dist/releases/download/v0.4.2/riotx-standalone-0.4.2-linux-aarch64.zip"
     sha256 "bdfdb640890074ebce575ef091604b212dd3affa5924afa19e05afad2c20de1a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/redis-field-engineering/riotx/releases/download/v0.4.2/riotx-standalone-0.4.2-linux-x86_64.zip"
+    url "https://github.com/redis-field-engineering/riotx-dist/releases/download/v0.4.2/riotx-standalone-0.4.2-linux-x86_64.zip"
     sha256 "bf584f0fb214a4b699ea8fbfc174b9d8c2a0c1de7bdb97d1f79ddb60877fcd43"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/redis-field-engineering/riotx/releases/download/v0.4.2/riotx-standalone-0.4.2-osx-aarch64.zip"
+    url "https://github.com/redis-field-engineering/riotx-dist/releases/download/v0.4.2/riotx-standalone-0.4.2-osx-aarch64.zip"
     sha256 "c962a052154ba10e6fc25de32b200c102e7b2bb6ddc95796fa957987748d078c"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/redis-field-engineering/riotx/releases/download/v0.4.2/riotx-standalone-0.4.2-osx-x86_64.zip"
+    url "https://github.com/redis-field-engineering/riotx-dist/releases/download/v0.4.2/riotx-standalone-0.4.2-osx-x86_64.zip"
     sha256 "48dc6f7610b40838407464f3e57088a6692f84b94a36bb2bde89dc7df119c012"
   end
 

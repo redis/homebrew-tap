84fd6c7463f39945b41c910b82a160063856fe0d
jreleaserbot <jreleaser@kordamp.org>
2025-09-04 06:41:09 +0000

    riotx v1.5.1

--- Formula/riotx.rb
+++ Formula/riotx.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.20.0 at 2025-09-01T00:05:52.376127506Z
+# Generated with JReleaser 1.20.0 at 2025-09-04T06:37:17.552312031Z
 
 class Riotx < Formula
   desc "Get data in and out of Redis with RIOT-X"
   homepage "https://github.com/redis/riotx-dist"
-  url "https://github.com/redis/riotx-dist/releases/download/v1.5.0/riotx-1.5.0.zip"
-  version "1.5.0"
-  sha256 "66f4f310fd10bf9cf16e4d06eee6521d67e7f2b568fbabb6ed28a8c6757622ed"
+  url "https://github.com/redis/riotx-dist/releases/download/v1.5.1/riotx-1.5.1.zip"
+  version "1.5.1"
+  sha256 "67d69e0321cf339f494beaa5d5884b8ccd2ce7bac9d3acb554d7478ee963a214"
   license "Apache-2.0"
 
   depends_on "openjdk@21"
@@ -17,6 +17,6 @@ class Riotx < Formula
 
   test do
     output = shell_output("#{bin}/riotx --version")
-    assert_match "1.5.0", output
+    assert_match "1.5.1", output
   end
 end

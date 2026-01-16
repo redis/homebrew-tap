0c077d082a81d93fe170933f766faff4e6b4b32a
jreleaserbot <jreleaser@kordamp.org>
2026-01-16 07:04:26 +0000

    riotx v1.7.8

--- Formula/riotx.rb
+++ Formula/riotx.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.22.0 at 2026-01-16T06:41:54.713613821Z
+# Generated with JReleaser 1.22.0 at 2026-01-16T07:00:16.1132029Z
 
 class Riotx < Formula
   desc "Get data in and out of Redis with RIOT-X"
   homepage "https://github.com/redis/riotx"
   url "https://github.com/redis/riotx-dist/releases/download/v1.7.8/riotx-1.7.8.zip"
   version "1.7.8"
-  sha256 "70178b921914221c7c534ab8494b2e6f21d4e2213f9326bab74bf66eba3580e5"
+  sha256 "6d3b326270feaca330f35ff17ab1c69ba91c37533d52677dff0fb0cfa7a90d03"
   license "Apache-2.0"
 
   depends_on "openjdk@17"

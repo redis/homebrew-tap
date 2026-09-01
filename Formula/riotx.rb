514fb7054caa75071d6f31024bf5e43c9564b2d8
Julien Ruaux <jruaux@gmail.com>
2026-09-01 23:12:02 +0200

    fix(riotx): use OpenJDK 17 launcher

--- Formula/riotx.rb
+++ Formula/riotx.rb
@@ -6,17 +6,19 @@ class Riotx < Formula
   url "https://github.com/redis/riotx-dist/releases/download/v1.14.2/riotx-1.14.2.zip"
   version "1.14.2"
   sha256 "a4f26c6d2727a7af42c8e9245e45ce18a78739c4acce6858d970fc0039a6fa93"
+  revision 1
   license "Apache-2.0"
 
   depends_on "openjdk@17"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/riotx" => "riotx"
+    (bin/"riotx").write_env_script libexec/"bin/riotx", Language::Java.overridable_java_home_env("17")
   end
 
   test do
     output = shell_output("#{bin}/riotx --version")
     assert_match "1.14.2", output
+    assert_match "JVM:          17.", output
   end
 end
